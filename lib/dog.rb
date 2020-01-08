5f873abb7f62f50231443a449da13131b12903ee
Bryan Rhodunda <rhodunda@udel.edu>
2020-01-07 20:41:56 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog 
+
+def name
+  @name  
+end 
+
+def name=(name)
+@name = name 
+end 
+
+def breed 
+    @breed
+end 
+
+def breed=(name_of_breed)
+    @breed = name_of_breed
+
+end 
+
+end 
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person 
+
+def name 
+    @name
+end 
+
+def name=(your_name)
+    @name = your_name 
+
+end 
+
+def job 
+    @job 
+end 
+
+def job=(your_job)
+    @job = your_job
+
+end 
+
+
+
+end 
