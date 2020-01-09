76486efb184cb8773a672d31c80fd8546c6d1f84
Trey-code <keytonwomack@yahoo.com>
2020-01-09 13:38:36 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+    attr_accessor :name, :job
+    def name 
+        @name 
+    end
+
+    def name=(input)
+        @name=input
+    end
+    
+    def job
+        @job
+    end
+
+    def job=(input)
+        @job=input
+    end
+end
